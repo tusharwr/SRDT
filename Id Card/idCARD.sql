733d9d2c5d9cac98be2a116a553e1509fa1c5fd9
Tushar Singh <tshrsngh@gmail.com>
2013-11-13 09:45:41 +0530

    All commnunication reports completed(all 4), ID card template modification template

--- Id Card/idCARD.sql
+++ Id Card/idCARD.sql
@@ -1,101 +1,35 @@
-SELECT A.EMPLID, A.ACAD_PROG, A.ADMIT_TERM, B.ACAD_PLAN, C.ACAD_YEAR, D.DESCR, D.ACAD_PLAN_TYPE, D.DEGREE
-  FROM PS_ACAD_PROG A, PS_ACAD_PLAN B, PS_TERM_TBL C, PS_ACAD_PLAN_TBL D
-  WHERE ( A.EFFDT =
-        (SELECT MAX(A_ED.EFFDT) FROM PS_ACAD_PROG A_ED
-        WHERE A.EMPLID = A_ED.EMPLID
-          AND A.ACAD_CAREER = A_ED.ACAD_CAREER
-          AND A.STDNT_CAR_NBR = A_ED.STDNT_CAR_NBR
-          AND A_ED.EFFDT <= SYSDATE)
-    AND A.EFFSEQ =
-        (SELECT MAX(A_ES.EFFSEQ) FROM PS_ACAD_PROG A_ES
-        WHERE A.EMPLID = A_ES.EMPLID
-          AND A.ACAD_CAREER = A_ES.ACAD_CAREER
-          AND A.STDNT_CAR_NBR = A_ES.STDNT_CAR_NBR
-          AND A.EFFDT = A_ES.EFFDT)
-     AND A.EMPLID = B.EMPLID
-     AND A.ACAD_CAREER = B.ACAD_CAREER
-     AND A.STDNT_CAR_NBR = B.STDNT_CAR_NBR
-     AND A.EFFSEQ = B.EFFSEQ
-     AND B.EFFDT =
-        (SELECT MAX(B_ED.EFFDT) FROM PS_ACAD_PLAN B_ED
-        WHERE B.EMPLID = B_ED.EMPLID
-          AND B.ACAD_CAREER = B_ED.ACAD_CAREER
-          AND B.STDNT_CAR_NBR = B_ED.STDNT_CAR_NBR
-          AND B_ED.EFFDT <= SYSDATE)
-    AND B.EFFSEQ =
-        (SELECT MAX(B_ES.EFFSEQ) FROM PS_ACAD_PLAN B_ES
-        WHERE B.EMPLID = B_ES.EMPLID
-          AND B.ACAD_CAREER = B_ES.ACAD_CAREER
-          AND B.STDNT_CAR_NBR = B_ES.STDNT_CAR_NBR
-          AND B.EFFDT = B_ES.EFFDT)
-     AND A.ACAD_CAREER = C.ACAD_CAREER
-     AND C.INSTITUTION = A.INSTITUTION
-     AND A.ADMIT_TERM = C.STRM
-     AND B.ACAD_PLAN = D.ACAD_PLAN
-     AND D.EFFDT =
-        (SELECT MAX(D_ED.EFFDT) FROM PS_ACAD_PLAN_TBL D_ED
-        WHERE D.INSTITUTION = D_ED.INSTITUTION
-          AND D.ACAD_PLAN = D_ED.ACAD_PLAN
-          AND D_ED.EFFDT <= SYSDATE) )
+SELECT DISTINCT A.EMPLID, A.CAMPUS_ID, A.ACAD_PROG, A.ADMIT_TERM, A.ACAD_PLAN, A.ACAD_YEAR, A.DESCR, A.ACAD_PLAN_TYPE
+, A.DEGREE, A.NAME_DISPLAY
+, A.ADDRESS_TYPE
+, A.COUNTRY
+,(case when a.address_type like('PERM') THEN  a.address1 ||' ' || a.address2 ||' ' || a.address3 ||' ' ||  a.city ||' ' || A.postal  END) AS PERMANENTADDR
+,(case when a.address_type like('HOME') THEN a.address1 ||' ' || a.address2 ||' ' || a.address3 ||' ' ||  a.city ||' ' || A.postal  END) AS HOMEADDR
+, A.ADDRESS1
+, A.ADDRESS2
+, A.ADDRESS3
+, A.ADDRESS4
+, A.PHONE_TYPE
+, A.CITY
+,(case when a.phone_type like ('CELL') THEN A.PHONE END) AS CELLPHONE 
+,(case when a.phone_type like ('HOME') THEN A.phone END) AS HOMEPHONE
+, A.PHONE
+, A.BLOOD_TYPE
+, A.POSTAL
+, A.PEOPLE_RELATION
+, A.FATHER_NAME
+, A.DESCR1
+, A.YEARS_OF_EDUCATN, A.L_BARCODE, A.NAME_PREFIX, A.NAME_PREFIX_U
+, (CASE WHEN substr(  A.ACAD_PROG,0,3)='BTR'  THEN (  A.DEGREE || ' (' || substr(  A.ACAD_PROG,4 ,2) || ')')
+   WHEN substr(  A.ACAD_PROG,0,3)='MTR' THEN 'M.TECH'
+   ELSE substr(  A.ACAD_PROG,0,3)
+   END)
+  FROM (PS_SRMU_STU_ID_VW A LEFT OUTER JOIN  PS_SRM_TUT_INC2_VW B ON  A.EMPLID = B.EMPLID )
+  WHERE ( A.EMPLID = '00000000157'
+  AND a.address_type='HOME' AND A.PHONE_TYPE='HOME' )
   ORDER BY 1;
   
-  -----------------------------------
-  select * from ps_SRMU_ST_PLAN_VW;
-  ------------------------------------------
-  SELECT A.EMPLID, A.NAME, A.NAME_DISPLAY, B.COUNTRY, B.ADDRESS1, B.ADDRESS2, B.ADDRESS3, B.ADDRESS4, B.CITY,B.POSTAL, C.PHONE, D.BLOOD_TYPE
-  FROM (((PS_PERSONAL_DATA A LEFT OUTER JOIN  PS_ADDRESSES B ON  A.EMPLID = B.EMPLID ) LEFT OUTER JOIN  PS_PERSONAL_PHONE C ON  A.EMPLID = C.EMPLID ) LEFT OUTER JOIN  PS_PHYSICAL_EXAM D ON  A.EMPLID = D.EMPLID )
-  WHERE ( B.EFFDT =
-        (SELECT MAX(B_ED.EFFDT) FROM PS_ADDRESSES B_ED
-        WHERE B.EMPLID = B_ED.EMPLID
-          AND B.ADDRESS_TYPE = B_ED.ADDRESS_TYPE
-          AND B_ED.EFFDT <= SYSDATE) );
-    ------------------------------------------      
-        select * FROM PS_SRMU_ST_DATA_VW;
-        
-        ------------------------------
-        select * from ps_relationships;
-        SELECT * FROM PS_ACAD_PROG;
-        ---------------------------------------
-        SELECT * FROM ps_srmu_stu_id_vw;
-        -------------------------------
-        
-        SELECT A.EMPLID 
- , A.ACAD_PROG 
- , A.ADMIT_TERM 
- , A.ACAD_PLAN 
- , A.ACAD_YEAR 
- , A.DESCR 
- , A.ACAD_PLAN_TYPE 
- , A.DEGREE 
- ,b.name_prefix 
- , B.NAME_DISPLAY 
- , B.COUNTRY 
- ,B.ADDRESS_TYPE
- , B.ADDRESS1 
- , B.ADDRESS2 
- , B.ADDRESS3 
- , B.ADDRESS4 
- , B.CITY 
- , b.phone_type
- , B.PHONE 
- , B.BLOOD_TYPE 
- , B.POSTAL 
- , C.PEOPLE_RELATION 
- , c.name_prefix AS NAME_PREFIX_U 
- , C.NAME AS FATHER_NAME 
- , D.DESCR AS DESCR1 
- , D.YEARS_OF_EDUCATN 
- , B.CAMPUS_ID 
-  FROM (((PS_SRMU_ST_PLAN_VW A LEFT OUTER JOIN PS_SRMU_ST_DATA_VW B ON A.EMPLID = B.EMPLID ) LEFT OUTER JOIN PS_RELATIONSHIPS C ON A.EMPLID = C.EMPLID ) LEFT OUTER JOIN PS_DEGREE_TBL D ON D.DEGREE = A.DEGREE ) 
- WHERE ( D.EFFDT = ( 
- SELECT MAX(D_ED.EFFDT) 
-  FROM PS_DEGREE_TBL D_ED 
- WHERE D.DEGREE = D_ED.DEGREE 
-   AND D_ED.EFFDT <= SYSDATE
-   and a.emplid='00000000689') );
-   
-   
-   select * from ps_srmu_stu_inf_vw;
-   
-   
+  SELECT * FROM PS_SRMU_STU_ID_VW;
   
+  SELECT EMPLID FROM ps_addresses WHERE address_type='PERM';
+  SELECT EMPLID, address_type FROM ps_addresses WHERE emplid='00000000157';
+  select * from ps_phone_type_xlvw ;
--- communication/Admission Fee Structure/AdmissionFeeStruct.sql
+++ communication/Admission Fee Structure/AdmissionFeeStruct.sql
@@ -2,9 +2,8 @@ SELECT A.EMPLID, A.ADMIT_TERM, A.ACAD_YEAR, A.NAME_DISPLAY, A.COUNTRY, A.ADDRESS
   FROM PS_SRMU_STU_ID_VW A;
   ---------------------------------------------------------------
   
-   SELECT * FROM PS_TERM_SUBFEE_TBL;
+   SELECT distinct * FROM PS_TERM_SUBFEE_TBL;
   SELECT * FROM PS_TERM_FE_CD_TBL;
-  select * from ps_acad_prog;
   select * from PS_STDNT_TEST_COMP;
   
   SELECT FEE_CODE, sub_fee_code, acad_PROG, flat_amt FROM PS_TERM_SUBFEE_TBL ;
@@ -12,40 +11,88 @@ SELECT A.EMPLID, A.ADMIT_TERM, A.ACAD_YEAR, A.NAME_DISPLAY, A.COUNTRY, A.ADDRESS
   select substr('abcdef',-2,2) from dual;
 
   ---------------------------
+  select * from PS_SRMU_STU_ID_VW where emplid='00000000249';
   select * from PS_SRMU_STU_ID_VW;
+  select * from ps_acad_prog;
+  select * from PS_TERM_SUBFEE_TBL where emplid='00000000157';
   -----------------------------------
-  SELECT DISTINCT C.EMPLID, A.FEE_CODE, A.STRM, A.FLAT_AMT, B.DESCR, C.ADMIT_TERM, C.ACAD_YEAR, C.NAME_DISPLAY, C.PEOPLE_RELATION, C.FATHER_NAME, C.CAMPUS_ID, C.ADDRESS1
-  , C.ADDRESS2, C.ADDRESS3, C.ADDRESS4, C.CITY, C.POSTAL
-  , (CASE WHEN substr( C.ACAD_PROG,0,3)='BTR' THEN ( C.DEGREE || ' (' || substr( C.ACAD_PROG,4 ,2) || ')') ELSE substr( C.ACAD_PROG,0,3) END) as "Degree"
-  FROM PS_TERM_SUBFEE_TBL A, PS_TERM_FE_CD_TBL B, PS_SRMU_STU_ID_VW C
-  WHERE ( B.FEE_CODE = A.FEE_CODE
-    AND B.SETID = A.SETID
-    AND ( A.STRM NOT LIKE '%02'
-     AND A.FEE_CODE = B.FEE_CODE
-     AND A.SETID = A.SETID
-     AND A.ACAD_PROG = C.ACAD_PROG
-     AND C.EMPLID = '00000001157'
-     AND substr( A.FEE_CODE,-2,2) = substr( C.ADMIT_TERM,1,2) ) )
-  ORDER BY 1, 3;
-  -------------------------------------------------------------------------
-   
-   SELECT DISTINCT C.EMPLID, A.FEE_CODE, 
-   A.STRM
-   , A.FLAT_AMT,
-   (case when b.descr like ('%Exam%') then 'Examination Fees'
-   when  b.descr like ('%Development%') then 'Development Fees'
-   when  b.descr like ('%Tuition%') then 'Tuition Fees'
-   end) as Description
-   ,C.ADMIT_TERM, C.ACAD_YEAR, C.NAME_DISPLAY, C.PEOPLE_RELATION, C.FATHER_NAME, C.CAMPUS_ID, C.ADDRESS1
-  , C.ADDRESS2, C.ADDRESS3, C.ADDRESS4, C.CITY, C.POSTAL
-  , (CASE WHEN substr( C.ACAD_PROG,0,3)='BTR' THEN ( C.DEGREE || ' (' || substr( C.ACAD_PROG,4 ,2) || ')') ELSE substr( C.ACAD_PROG,0,3) END) as "Degree"
-  FROM PS_TERM_SUBFEE_TBL A, PS_TERM_FE_CD_TBL B, PS_SRMU_STU_ID_VW C
-  WHERE ( B.FEE_CODE = A.FEE_CODE
-    AND B.SETID = A.SETID
-    AND ( A.STRM NOT LIKE '%02'
-     AND A.FEE_CODE = B.FEE_CODE
-     AND A.SETID = A.SETID
-     AND A.ACAD_PROG = C.ACAD_PROG
-     AND C.EMPLID = '00000001157'
-     AND substr( A.FEE_CODE,-2,2) = substr( C.ADMIT_TERM,1,2) ) )
-  ORDER BY 1, 3;
+  --------------------------------------------------------------------------------------------
+   	
+ 	 
+ SELECT DISTINCT C.EMPLID 
+ , A.FEE_CODE 
+ , A.STRM 
+ , ( CASE WHEN substr( C.ACAD_YEAR 
+ ,3 
+ ,2)=substr( A.STRM 
+ ,1 
+ ,2) THEN nvl( A.FLAT_AMT 
+ ,0) END) AS AMT1 
+ , ( CASE WHEN substr( C.ACAD_YEAR+1 
+ ,3 
+ ,2)=substr( A.STRM 
+ ,1 
+ ,2) THEN A.FLAT_AMT END) AS AMT2 
+ , ( CASE WHEN substr( C.ACAD_YEAR+2 
+ ,3 
+ ,2)=substr( A.STRM 
+ ,1 
+ ,2) THEN A.FLAT_AMT END) AS AMT3 
+ , ( CASE WHEN substr( C.ACAD_YEAR+3 
+ ,3 
+ ,2)=substr( A.STRM 
+ ,1 
+ ,2) THEN A.FLAT_AMT END) AS AMT4 
+ , ( CASE WHEN substr( C.ACAD_YEAR+4 
+ ,3 
+ ,2)=substr( A.STRM 
+ ,1 
+ ,2) THEN A.FLAT_AMT END) AS AMT5 
+ , (CASE WHEN B.DESCR LIKE ('%Exam%') THEN 'Examination Fees' WHEN B.DESCR LIKE ('%Development%') THEN 'Development Fees' WHEN B.DESCR LIKE ('%Tuition%') THEN 'Tuition Fees' END) AS DESCR 
+ , C.ADMIT_TERM 
+ , C.ACAD_YEAR 
+ , C.NAME_DISPLAY 
+ , C.PEOPLE_RELATION 
+ , C.FATHER_NAME 
+ , C.CAMPUS_ID 
+ , C.ADDRESS_TYPE 
+ , C.ADDRESS1 
+ , C.ADDRESS2 
+ , C.ADDRESS3 
+ , C.ADDRESS4 
+ , C.CITY 
+ , C.POSTAL 
+ , (CASE WHEN substr( C.ACAD_PROG 
+ ,0 
+ ,3)='BTR' 
+    OR substr( C.ACAD_PROG 
+    ,0 
+    ,3)='MTR' THEN ( C.DEGREE || ' (' || substr( C.ACAD_PROG 
+    ,4 
+    ,2) || ')') WHEN substr( C.ACAD_PROG 
+    ,0 
+    ,3)='MTR' THEN 'M.TECH' ELSE substr( C.ACAD_PROG 
+    ,0 
+    ,3) END) AS DEGREE25 
+    , D.TEST_ID 
+    , nvl( D.SCORE_LETTER 
+    ,'N/A') AS RANK 
+    ,(case when substr(C.acad_year,3,2)=substr(A.strm,1,2) then '1st Year'
+  when substr(C.acad_year+1,3,2)=substr(A.strm,1,2) then '2nd Year'
+  when substr(C.acad_year+2,3,2)=substr(A.strm,1,2) then '3rd Year'
+  when substr(C.acad_year+3,3,2)=substr(A.strm,1,2) then '4th Year'
+  when substr(C.acad_year+4,3,2)=substr(A.strm,1,2) then '5th Year'
+ END) as Year
+  FROM PS_TERM_SUBFEE_TBL A 
+  , PS_TERM_FE_CD_TBL B 
+  , (PS_SRMU_STU_ID_VW C LEFT OUTER JOIN PS_STDNT_TEST_COMP D ON C.EMPLID = D.EMPLID ) 
+ WHERE ( B.FEE_CODE = A.FEE_CODE 
+   AND B.SETID = A.SETID 
+   AND ( A.STRM NOT LIKE '%02' 
+   AND A.FEE_CODE = B.FEE_CODE 
+   AND A.SETID = A.SETID 
+   AND A.ACAD_PROG = C.ACAD_PROG 
+   AND substr( A.FEE_CODE,-2,2) = substr( C.ADMIT_TERM,1,2) 
+   AND C.ADDRESS_TYPE = 'PERM' 
+   AND C.EMPLID='00000000157') ) 
+  ORDER BY 1, 3, 8;
